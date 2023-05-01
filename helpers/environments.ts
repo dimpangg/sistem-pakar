381753262b7484e4aff1f199edb503ea9052ebab
Dimas Pangestu <dimaspangestu19@gmail.com>
2023-05-01 19:08:03 +0700

    set env

--- helpers/environments.ts
+++ helpers/environments.ts
@@ -1,6 +1,5 @@
+import { env } from "@/env.mjs";
+
 export const SECRET_KEY =
-  process.env.APP_SECRET_KEY ||
   "16aba5393ad72c0041f5600ad3c2c52ec437a2f0c7fc08fadfc3c0fe9641d7a3";
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ||
-  "https://simop.mitrajamur.com/api/expert-system";
+export const API_URL = `${env.NEXT_PUBLIC_API_URL}/api/expert-system`;
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
+import "./env.mjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
